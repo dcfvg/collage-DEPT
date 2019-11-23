260f7fda8e47efa9581413fea6ff5ee95b62dd13
dcfvg <qwsx@dcfvg.com>
2019-11-23 19:13:47 +0100

    drag upt state

--- src/components/CollageViewer.vue
+++ src/components/CollageViewer.vue
@@ -1,52 +1,20 @@
 <template>
   <div class="flat-background">
     <img class="flat-img" :src=flatImgUrl>
-
     <div id="cuts">
       <div v-for="item in state.cuts" v-bind:key="item.id">
-        <vue-draggable-resizable class="drag"
-        :t=item.id
-        :x=item.x
-        :y=item.y
-        :w=item.w
-        :h=item.h
-        :lock-aspect-ratio="true"
-        @dragging="onDrag"
-        @resizing="onResize"
-        >
-          <img :src="'http://localhost:8080/crops/'+item.id+'.jpg'">
-        </vue-draggable-resizable>
+          <DraggableResizableCut :item=item ></DraggableResizableCut>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
 import state from './../state.js'
+import DraggableResizableCut from './DraggableResizableCut.vue'
 export default {
   name: 'SelectFlat',
   data: function(){ return {state} },
-  methods: {
-    onResize: function (x,y,w,h,t) {
-
-      // let newCut = state.data.cuts.find((e) => {return e.id == cutId });
-      // console.log(cutId);
-      console.log(x,y,w,h, t, this)
-
-      this.x = x
-      this.y = y
-      this.width = w
-      this.height = h
-      //
-      // console.log(this);
-    },
-    onDrag: function (x,y) {
-            console.log(x,y)
-      this.x = x
-      this.y = y
-    }
-  },
   computed: {
     flatImgUrl() {
       var found = state.data.flats.find(function(element) {
@@ -57,6 +25,9 @@ export default {
       if (typeof found !== 'undefined') return "http://localhost:8080/flats/"+found.name;
       else return "none";
     }
+  },
+  components: {
+    DraggableResizableCut
   }
 }
 </script>
--- src/components/DraggableResizableCut.vue
+++ src/components/DraggableResizableCut.vue
@@ -1,32 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+    <vue-draggable-resizable class="drag"
+    :x=item.x
+    :y=item.y
+    :w=item.w
+    :h=item.h
+    :lock-aspect-ratio="true"
+    @dragstop="onDragStop"
+    @resizestop="onResizeStop"
+    >
+      <img :src="'http://localhost:8080/crops/'+item.id+'.jpg'">
+    </vue-draggable-resizable>
 </template>
 
 <script>
+
+import state from './../state.js'
+
 export default {
-  name: 'HelloWorld',
+  name: 'DraggableResizableCut',
   props: {
-    msg: String
+    item: Object,
+    state: function(){ return {state} }
+  },
+  methods: {
+    onResizeStop: function (x,y,w,h) {
+      let cutIndex = state.cuts.findIndex((e) => {return e.id == this.$props.item.id });
+      state.cuts[cutIndex].w = w
+      state.cuts[cutIndex].h = h
+    },
+    onDragStop: function (x,y) {
+      let cutIndex = state.cuts.findIndex((e) => {return e.id == this.$props.item.id });
+      state.cuts[cutIndex].x = x
+      state.cuts[cutIndex].y = y
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
